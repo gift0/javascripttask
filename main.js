463e54f4aae5ad4f34f973baf90cc9f367e89e30
Your Name <you@example.com>
2020-03-26 10:40:30 +0100

    pushing my javascript task

---
+++ main.js
@@ -0,0 +1,15 @@
+let myName= "gift0";
+let myCourse= ["html", "css", "javascript"]
+for (let i = 0; i < myCourse.length; i++) {
+    console.log(myCourse[i]);
+}
+console.log(`my name is ${myName}` );
+if (myCourse.length % 2 === 0) {
+    for (let i = 0; i <= 200; i++) {
+        if (i%2===0) {
+            console.log(i);
+        }
+
+    }
+
+}
